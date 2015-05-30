d892305aaa7ca9ec654a40d47989e133665c8b26
Rimon <rimonchi@rimons-imac-3.local>
2015-05-30 15:42:51 +0800

    Add happy numbers

---
+++ src/my_rand_prj/happy_num.clj
@@ -0,0 +1,30 @@
+(ns happy)
+
+(defn digit-seq [x]
+  (let [base 10]
+    (if (>= x base)
+      (let [rem (rem x base)
+            i   (int (/ x base))]
+        (cons rem (lazy-seq (digit-seq i))))
+      (list x))))
+
+(defn next-val [x]
+  (->> (digit-seq x)
+       (map #(* % %))
+       (reduce +)))
+
+(defn num-seq [x]
+  (cons x (lazy-seq (num-seq (next-val x)))))
+
+(defn happy? [x]
+  (loop [seen #{}
+         xs   (num-seq x)]
+    (let [e  (first xs)]
+      (cond
+        (= e 1)             true
+        (contains? seen e)  false
+        :else               (recur (conj seen e) (rest xs))))))
+
+(def happy-numbers (filter happy? (iterate inc 1)))
+
+;;(take 20 happy-numbers)
