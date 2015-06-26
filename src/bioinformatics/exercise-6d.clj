bb798bce6d619d33ddf6034411c7f5869a82edf6
Rimon <rimonchi@rimons-imac-3.local>
2015-06-26 23:54:42 +0800

    updates

--- src/bioinformatics/exercise-6d.clj
+++ src/bioinformatics/exercise-6d.clj
@@ -9,32 +9,36 @@
          (mapv dna-complement))))
 
 (defn shared-kmers-problems [k s1 s2]
-  (letfn [(get-pair [l1 l2]
-            (when-let [[a i1] (first l1)]
-              (when-let [[b i2] (first l2)]
-                (condp #(%1 %2 0) (compare a b)
-                  =  (cons [i1 i2]
-                           (lazy-seq (get-pair (rest l1) (rest l2))))
-                  <  (lazy-seq (get-pair (rest l1) l2))
-                  >  (lazy-seq (get-pair l1 (rest l2)))))))
-          (gen-seq [genome f]
+  (letfn [(gen-seq [genome f]
             (->> (partition k 1 genome)
                  (map-indexed (fn [i e] [(f e) i]))
-                 (sort-by first)))]
-    (let [l1 (gen-seq s1 vec)
-          l2 (gen-seq s2 vec)
-          l3 (gen-seq s2 (comp reverse-complement vec))]
-      (->> (get-pair l1 l2)
+                 (sort-by first)))
+          (drop-elements [x xs]
+            (drop-while (fn [[e _]] (= e x)) xs))
+          (get-pairs [xs ys]
+            (when-let [[a ai] (first xs)]
+              (when-let [[b bi] (first ys)]
+                (condp #(%1 %2 0) (compare a b)
+                  <  (lazy-seq (get-pairs (rest xs) ys))
+                  >  (lazy-seq (get-pairs xs (rest ys)))
+                  =  (let [matches (for [[x xi] xs :while (= a x)
+                                         [y yi] ys :while (= a y)]
+                                     [xi yi])]
+                       (concat matches
+                               (lazy-seq (get-pairs (drop-elements a xs)
+                                                    (drop-elements a ys)))))))))]
+    (let [xs (gen-seq s1 vec)
+          ys (gen-seq s2 vec)
+          rs (gen-seq s2 (comp reverse-complement vec))]
+      (->> (get-pairs xs ys)
            (map (fn [[x y]] [x 0 y]))
-           (concat (->> (get-pair l1 l3)
+           (concat (->> (get-pairs xs rs)
                         (map (fn [[x y]] [x 1 y]))))
            sort
            (map (fn [[x _ z]] [x z]))
            (cl-format nil "~{(~{~a~^, ~})~%~}")))))
 
-(shared-kmers-problems 3 "AAACTCATCATC" "TTTCAAATC")
-
-(def path #(str "./" %))
+(def path #(str (System/getProperty "user.home") "/Downloads/" %))
 (def in-file (path "rosalind_6d.txt"))
 (def out-file (path "rosalind_6d_output.txt"))
 
@@ -43,31 +47,33 @@
       s1   (nth txt 1)
       s2   (nth txt 2)]
 
-  (->> (shared-kmers-problems k s1 s2)
+  (->> (time (shared-kmers-problems k s1 s2))
        (spit out-file)))
 
 (let [txt  (clojure.string/split-lines (slurp in-file))
       k    (read-string (first txt))
       s1   (nth txt 1)
       s2   (nth txt 2)
-      l1   (vec (partition k 1 s1))
-      l2   (vec (partition k 1 s2))
+      xs   (vec (partition k 1 s1))
+      ys   (vec (partition k 1 s2))
       out  (->> (slurp out-file)
                 (clojure.string/split-lines)
                 (map read-string))
       s    #(apply str %)]
 
-  (->> out
-       (map (fn [[x y]]
-              (let [a      (s (nth l1 x))
-                    b      (s (nth l2 y))
-                    c      (s (reverse-complement a))
-                    a-eq-b (= a b)
-                    b-eq-c (= b c)]
-                [x y a b c a-eq-b b-eq-c (or a-eq-b b-eq-c)])))
-       #_(filter #(nth % 5)) ; exact match
-       #_(filter #(nth % 6)) ; matches reverse-complement
-       (filter #(not (last %))) ; not match?
-       count
-       pprint
-       #_(cl-format "~{~{~a~^, ~}~}")))
+  (let [results (map (fn [[x y]]
+                       (let [a      (s (nth xs x))
+                             b      (s (nth ys y))
+                             c      (s (reverse-complement a))
+                             a-eq-b (= a b)
+                             b-eq-c (= b c)]
+                         [x y a b c a-eq-b b-eq-c (or a-eq-b b-eq-c)]))
+                     out)
+        exact-match  (filter #(nth % 5) results)
+        revert-match (filter #(nth % 6) results)
+        no-match     (filter #(not (last %)) results)
+        ]
+    (cl-format *out* "Exact matches: ~d~%Revert matches: ~d~%No match: ~d"
+               (count exact-match)
+               (count revert-match)
+               (count no-match))))
