2fea31b8810e16772b9450f9f8756d9d4f25517a
Rimon <rimonchi@gmail.com>
2015-06-30 08:01:34 +0800

    update tests

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Some random projects to play with...
 
 For Mac user, please build and install https://github.com/conormcd/osx-keychain-java to local Maven repo
-# git clone git@github.com:conormcd/osx-keychain-java.git
-# ant jar
-# mvn install:install-file -Dfile=dist/osxkeychain.jar -DgroupId=com.mcdermottroe.apple -DartifactId=osxkeychain -Dversion=1.0 -Dpackaging=jar
+
+git clone git@github.com:conormcd/osx-keychain-java.git
+ant jar
+mvn install:install-file -Dfile=dist/osxkeychain.jar -DgroupId=com.mcdermottroe.apple -DartifactId=osxkeychain -Dversion=1.0 -Dpackaging=jar
 
 ## Usage
 
--- test/bioinformatics/utils_test.clj
+++ test/bioinformatics/utils_test.clj
@@ -3,11 +3,27 @@
   (:use bioinformatics.utils))
 
 
-(fact (into [] (running-count #(= % \a)) "aabbcCcddeeggffabcd") => [1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3])
+(fact
+ (into [] (running-count #(= % \a)) "aabbcCcddeeggffabcd")
+ => [1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3])
 
-(fact (into [] (comp (running-count #(= % \a))
-                     (remove #(= 1 %))) "aabbcCcddeeggffabcd") => [2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3])
+(fact
+ (into [] (comp (running-count #(= % \a))
+                (remove #(= 1 %))
+                (filter even?)) "aabbcCcddeeggffabcd")
+ => [2 2 2 2 2 2 2 2 2 2 2 2 2 2])
 
-(fact (sequence (running-count #(= % \a)) "aabbcCcddeeggffabcd") => '(1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3))
+(fact
+ (sequence (running-count #(= % \a)) "aabbcCcddeeggffabcd")
+ => '(1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3))
 
-(fact (into [] (count-same) "12233344445666667") => ['(\1 1) '(\2 2) '(\3 3) '(\4 4) '(\5 1) '(\6 5) '(\7 1)])
+(fact
+ (into [] (count-same) "12233344445666667")
+ => ['(\1 1) '(\2 2) '(\3 3) '(\4 4) '(\5 1) '(\6 5) '(\7 1)])
+
+
+(fact
+ (into [] (comp (running-count #(= % \a))
+                (count-same)
+                (map last)) "aabbcCcddeeggffabcd")
+ => [1 14 4])
