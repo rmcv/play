976ebf92cebb94aca113d77044fdaa5e79508879
Rimon <rimonchi@gmail.com>
2015-07-12 22:28:57 +0800

    perf tune - wip

--- src/bioinformatics/nwa.clj
+++ src/bioinformatics/nwa.clj
@@ -1,12 +1,12 @@
 (ns bioinformatics.nwa
   (:require [clojure.string :as str])
   (:use [bioinformatics.utils]
-        [clojure.pprint]))
+        [clojure.pprint]
+        [criterium.core]))
 
 
 (def BLOSUM62
-  (let [m
-"A  C  D  E  F  G  H  I  K  L  M  N  P  Q  R  S  T  V  W  Y
+  "A  C  D  E  F  G  H  I  K  L  M  N  P  Q  R  S  T  V  W  Y
 A  4  0 -2 -1 -2  0 -2 -1 -1 -1 -1 -2 -1 -1 -1  1  0  0 -3 -2
 C  0  9 -3 -4 -2 -3 -3 -1 -3 -1 -1 -3 -3 -3 -3 -1 -1 -1 -2 -2
 D -2 -3  6  2 -3 -1 -1 -3 -1 -4 -3  1 -1  0 -2  0 -1 -3 -4 -3
@@ -27,38 +27,51 @@ T  0 -1 -1 -1 -2 -2 -2 -1 -1 -1 -1  0 -1 -1 -1  1  5  0 -2 -2
 V  0 -1 -3 -2 -1 -3 -3  3 -2  1  1 -3 -2 -2 -3 -2  0  4 -3 -1
 W -3 -2 -4 -3  1 -2 -2 -3 -3 -2 -1 -4 -4 -2 -3 -3 -2 -3 11  2
 Y -2 -2 -3 -2  3 -3  2 -1 -2 -1 -1 -2 -3 -1 -2 -2 -2 -1  2  7"
-        [h & d] (clojure.string/split-lines m)
-        cs (map first (clojure.string/split h #"  +"))
-        pl #(let [[[y & _] & d] (clojure.string/split % #" +")]
-              (zipmap (map vector (repeat y) cs) (map read-string d)))]
-    (->> (map pl d)
-         (apply merge))))
+)
 
-(def sub-score BLOSUM62)
+
+(defn sub-score* [matrix]
+  (let [[h & d] (str/split-lines matrix)
+        cs      (zipmap (map first (str/split h #"  +"))
+                        (range))
+        dm      (mapv (fn [l] (->> (str/split l #" +")
+                                  (drop 1)
+                                  (mapv read-string))) d)
+        ]
+    (fn [a b]
+      (let [i (cs a)
+            j (cs b)]
+        (nth (nth dm i) j))))
+  )
+
+(def sub-score (sub-score* BLOSUM62))
 
 (def gap -5)
 
-(defn score [x y i j]
+(defrecord Res [score source])
+
+(defn score  [x y i j]
   (let [top?  (= i 0)
         left? (= j 0)]
     (if (or top? left?)
       (let [s (* gap (+ i j))]
-        (cond
-          (and top? left?) [s [:done]]
-          top?             [s [:l]]
-          left?            [s [:t]]))
-      (let [[d dv] (score x y (dec i) (dec j))
-            [t tv] (score x y (dec i) j)
-            [l lv] (score x y i (dec j))
-            [p s] (->> {:d (+ d (sub-score [(nth y i) (nth x j)]))
-                        :l (+ l gap)
-                        :t (+ t gap)}
-                       (apply max-key val))
-            v     (cons p (condp = p
-                            :d dv
-                            :l lv
-                            :t tv))]
-        [s v]))))
+        (Res. s (cond
+                  (and top? left?) :done
+                  top?             :l
+                  left?            :t)))
+      (let [{d :score}  (score x y (dec i) (dec j))
+            {t :score}  (score x y (dec i) j)
+            {l :score}  (score x y i (dec j))
+            dv          (+ d (sub-score (nth y i) (nth x j)))
+            tv          (+ t gap)
+            lv          (+ l gap)
+            m           (max dv tv lv)
+            source      (condp = m
+                          dv :d
+                          tv :t
+                          lv :l)
+            ]
+        (Res. m source)))))
 
 
 (defn track-back [x y path]
@@ -75,24 +88,27 @@ Y -2 -2 -3 -2  3 -3  2 -1 -2 -1 -1 -2 -3 -1 -2 -2 -2 -1  2  7"
             path)))
 
 (defn global-align [x y]
-  (let [x (cons \$ x)
-        y (cons \$ y)]
+  (let [x (vec (cons \$ x))
+        y (vec (cons \$ y))]
     (with-redefs [score (memoize score)]
       (let [ans (last (for [i (range (count y))
                             j (range (count x))]
-                        (score x y i j)))]
+                        (score  x y i j)))]
         (-> ans
-            (update 1 #(track-back x y %)))))))
+            #_(update 1 #(track-back x y %)))))))
 
 (let [[input output] (io "rosalind_5e")
       [a b] input
-      [val path] (time (global-align a b))
-      pf    #(cl-format nil "~{~a~}" (map (fn [x] (if x x \-)) %))
-      s1    (pf (map first path))
-      s2    (pf (map last path))
-      s     (str val "\n" s1 "\n" s2)
+      a (take 4000 a)
+      b (take 500 b)
+      r (time (global-align a b))
+      ;pf    #(cl-format nil "~{~a~}" (map (fn [x] (if x x \-)) %))
+      ;s1    (pf (map first path))
+      ;s2    (pf (map last path))
+      ;s     (str val "\n" s1 "\n" s2)
       ]
-  (output s))
+  r
+  #_(output s))
 
 (time (global-align "SEND" "AND"))
 
